078237028da7c7b2d0cdb368de1912cd00996652
Nancy Irisarri <nancyirisarri@gmail.com>
2017-04-25 15:33:33 +0200

    move files to a week1 directory

--- plot1.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-
-data <- fread("household_power_consumption.txt")
-data <- data[which(data$Date=="1/2/2007" | data$Date == "2/2/2007"),]
-
-png(filename="plot1.png")
-with(data, hist(as.numeric(Global_active_power), col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)"))
-
-dev.off()
